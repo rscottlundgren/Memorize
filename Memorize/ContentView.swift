f6c8a045bb629858de66a63a42753d9f741b4042
rscottlundgren <rscottlundgren@gmail.com>
2020-08-18 14:04:20 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Richard Scott Lundgren on 8/18/20.
+//  Copyright © 2020 Richard Scott Lundgren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
